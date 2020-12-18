7c9de782067254b9de2a7f6cd19effae134001b2
lfarul <LFarulewski@yahoo.com>
2020-12-18 19:51:05 +0100

    Removed Test project and added a test project inside the main project.

---
+++ XUnitTestProject/BasicTest.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Dental_App;
+
+namespace XUnitTestProject
+{
+    public class BasicTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        public BasicTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Home")]
+        [InlineData("/Home/About")]
+        [InlineData("/Account/Register")]
+        [InlineData("/Account/Login")]
+        [InlineData("/Employee/List")]
+        [InlineData("/Employee/NewList")]
+        [InlineData("/Appointment/CreateAppointment")]
+        [InlineData("/Appointment/GetAllAppointments")]
+        public async Task GetHttpRequest(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task Index_WhenCalled_ReturnsApplicationForm()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/WebApi/GetUsers");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Joanna", responseString);
+            Assert.Contains("Lineker", responseString);
+        }
+    }
+}
